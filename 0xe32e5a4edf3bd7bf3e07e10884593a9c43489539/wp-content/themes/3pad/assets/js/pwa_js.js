5600de6623cfb9553a1b13ac57a36b1568d80520
3Pad <124752275+3Pad@users.noreply.github.com>
2023-04-05 02:45:29 -0400

    Test Delete

--- 0xe32e5a4edf3bd7bf3e07e10884593a9c43489539/wp-content/themes/3pad/assets/js/pwa_js.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener('install',function(e){console.log('Service worker installation calling!');});self.addEventListener('activate',function(e){console.log('Service worker activation calling!');});self.addEventListener('fetch',function(e){console.log('Service worker fetch calling!');if(!e.request.url.match(/^(http|https):\/\//i))
-return;if(new URL(e.request.url).origin!==location.origin)
-return;});
